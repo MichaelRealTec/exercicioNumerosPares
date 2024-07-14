379e0f235e19bf8a1deda2b880674910e09a620f
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 14:51:45 -0300

    Primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Problema numeros_pares
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		
+		
+		sc.close();
+
+	}
+
+}
