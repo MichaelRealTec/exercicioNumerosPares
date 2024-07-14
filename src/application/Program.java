198fbf0b27b7261376bb67b3942c4c5372021dd7
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 14:59:18 -0300

    exercicio resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,13 +7,32 @@ public class Program {
 
 	public static void main(String[] args) {
 		// Problema numeros_pares
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
+		int n, qtdNumPar = 0;
+
 		System.out.print("Quantos numeros voce vai digitar? ");
+		n = sc.nextInt();
+		int[] vectNum = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um numero: ");
+			vectNum[i] = sc.nextInt();
+		}
 		
-		
+		System.out.println();
+		System.out.println("NUMEROS PARES: ");
+		for (int i = 0; i < n; i++) {
+			if (vectNum[i] % 2 == 0) {
+				System.out.printf("%d ", vectNum[i]);
+				qtdNumPar++;
+			}
+		}
+		System.out.println();
+		System.out.println();
+		System.out.printf("QUANTIDADE DE PARES = %d",qtdNumPar);
 		sc.close();
 
 	}
